d424b462823ee3c743c1950768d577d4b9dbb69c
Ethan Olsen <ethan@crowdhubapps.com>
2021-06-17 16:23:48 -0700

    init

---
+++ src/lib/Icon/index.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Facebook from './Facebook.svelte';
+
+	let icons = {
+		facebook: Facebook,
+		linkedin: '',
+		instagram: ''
+	};
+
+	export let iconType: keyof typeof icons;
+</script>
